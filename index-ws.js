ed1cacbf15062b9aae66389475dbbc81ceef8799
zak aden <s.aden159@gmail.com>
2023-07-23 01:38:51 +0100

    add db logging

--- index-ws.js
+++ index-ws.js
@@ -13,6 +13,15 @@ server.listen(3000, () => {
   console.log('listening on port 3000');
 })
 
+process.on('SIGINT', () => {
+  wss.clients.forEach((client) => {
+    client.close()
+  })
+  server.close(() => {
+    shutdownDB()
+  })
+})
+
 /** WebSockets */
 const webSocketServer = require("ws").Server;
 
@@ -35,8 +44,37 @@ wss.on('connection', (ws) => {
     ws.send('welcome to my server')
   }
 
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))
+  `)
+
   ws.on('close', () => {
     wss.broadcast(`current visitors ${numClients}`)
     console.log('client has disconnected');
   })
 })
+
+/** Begin database */
+const sql = require('sqlite3')
+const db = new sql.Database(':memory:')
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `)
+})
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  })
+}
+
+function shutdownDB () {
+  getCounts()
+  console.log('shutting down db..');
+  db.close()
+}
